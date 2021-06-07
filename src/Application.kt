ccef23fba4fd796e9987da2659a22a15f258e089
castasat <castasat@mail.ru>
2021-06-07 17:26:07 +0300

    refactoring4 - refactoring of Application

--- src/Application.kt
+++ src/Application.kt
@@ -27,35 +27,11 @@ fun main(args: Array<String>) = EngineMain.main(args)
 @ExperimentalSerializationApi
 @JvmOverloads
 fun Application.module(testing: Boolean = false) {
-
     initDB()
-
     install(DefaultHeaders)
     install(CallLogging)
     install(ContentNegotiation) { json() }
-
-    di {
-        bindFirstNameRepository()
-        bindLastNameRepository()
-        bindGenderRepository()
-        bindBirthdayRepository()
-        bindCountryRepository()
-        bindRegionRepository()
-        bindLocalityTypeRepository()
-        bindLocalityRepository()
-        bindStreetRepository()
-        bindBuildingRepository()
-        bindApartmentRepository()
-        bindMetroStationRepository()
-        bindPatronymicRepository()
-        bindCoordinatesRepository()
-        bindEntranceRepository()
-        bindEntranceCodeRepository()
-        bindServiceTypeRepository()
-        bindPriceRepository()
-        // TODO bind data.repositories here
-    }
-
+    di { bindRepositories() }
     routing {
         rootRoute()
         firstNameRoute()
@@ -85,104 +61,53 @@ fun Application.initDB() {
     val dbConfig = HikariConfig(configPath)
     val dataSource = HikariDataSource(dbConfig)
     Database.connect(dataSource)
-
     createTables()
-
     LoggerFactory.getLogger(Application::class.simpleName).info("Initialized Database")
 }
 
 private fun createTables() = transaction {
-    SchemaUtils.create(FirstNamesTable)
-    SchemaUtils.create(LastNamesTable)
-    SchemaUtils.create(GendersTable)
-    SchemaUtils.create(BirthdaysTable)
-    SchemaUtils.create(CountriesTable)
-    SchemaUtils.create(RegionsTable)
-    SchemaUtils.create(LocalityTypesTable)
-    SchemaUtils.create(LocalitiesTable)
-    SchemaUtils.create(StreetsTable)
-    SchemaUtils.create(BuildingsTable)
-    SchemaUtils.create(ApartmentsTable)
-    SchemaUtils.create(MetroStationsTable)
-    SchemaUtils.create(PatronymicsTable)
-    SchemaUtils.create(CoordinatesTable)
-    SchemaUtils.create(EntrancesTable)
-    SchemaUtils.create(EntranceCodesTable)
-    SchemaUtils.create(ServiceTypesTable)
-    SchemaUtils.create(PricesTable)
-    // TODO create tables here
-}
-
-fun DI.MainBuilder.bindFirstNameRepository() {
-    bind<FirstNameRepository>() with singleton { FirstNameRepository() }
-}
-
-fun DI.MainBuilder.bindLastNameRepository() {
-    bind<LastNameRepository>() with singleton { LastNameRepository() }
-}
-
-fun DI.MainBuilder.bindGenderRepository() {
-    bind<GenderRepository>() with singleton { GenderRepository() }
+    with(SchemaUtils){
+        create(FirstNamesTable)
+        create(LastNamesTable)
+        create(GendersTable)
+        create(BirthdaysTable)
+        create(CountriesTable)
+        create(RegionsTable)
+        create(LocalityTypesTable)
+        create(LocalitiesTable)
+        create(StreetsTable)
+        create(BuildingsTable)
+        create(ApartmentsTable)
+        create(MetroStationsTable)
+        create(PatronymicsTable)
+        create(CoordinatesTable)
+        create(EntrancesTable)
+        create(EntranceCodesTable)
+        create(ServiceTypesTable)
+        create(PricesTable)
+        // TODO create tables here
+    }
 }
 
 @ExperimentalSerializationApi
-fun DI.MainBuilder.bindBirthdayRepository() {
+fun DI.MainBuilder.bindRepositories(){
+    bind<FirstNameRepository>() with singleton { FirstNameRepository() }
+    bind<LastNameRepository>() with singleton { LastNameRepository() }
+    bind<GenderRepository>() with singleton { GenderRepository() }
     bind<BirthdayRepository>() with singleton { BirthdayRepository() }
-}
-
-fun DI.MainBuilder.bindCountryRepository() {
     bind<CountryRepository>() with singleton { CountryRepository() }
-}
-
-fun DI.MainBuilder.bindRegionRepository() {
     bind<RegionRepository>() with singleton { RegionRepository() }
-}
-
-fun DI.MainBuilder.bindLocalityTypeRepository() {
     bind<LocalityTypeRepository>() with singleton { LocalityTypeRepository() }
-}
-
-fun DI.MainBuilder.bindLocalityRepository() {
     bind<LocalityRepository>() with singleton { LocalityRepository() }
-}
-
-fun DI.MainBuilder.bindStreetRepository() {
     bind<StreetRepository>() with singleton { StreetRepository() }
-}
-
-fun DI.MainBuilder.bindBuildingRepository() {
     bind<BuildingRepository>() with singleton { BuildingRepository() }
-}
-
-fun DI.MainBuilder.bindApartmentRepository() {
     bind<ApartmentRepository>() with singleton { ApartmentRepository() }
-}
-
-fun DI.MainBuilder.bindMetroStationRepository() {
     bind<MetroStationRepository>() with singleton { MetroStationRepository() }
-}
-
-fun DI.MainBuilder.bindPatronymicRepository() {
     bind<PatronymicRepository>() with singleton { PatronymicRepository() }
-}
-
-fun DI.MainBuilder.bindCoordinatesRepository() {
     bind<CoordinatesRepository>() with singleton { CoordinatesRepository() }
-}
-
-fun DI.MainBuilder.bindEntranceRepository() {
     bind<EntranceRepository>() with singleton { EntranceRepository() }
-}
-
-fun DI.MainBuilder.bindEntranceCodeRepository() {
     bind<EntranceCodeRepository>() with singleton { EntranceCodeRepository() }
-}
-
-fun DI.MainBuilder.bindServiceTypeRepository() {
     bind<ServiceTypeRepository>() with singleton { ServiceTypeRepository() }
-}
-
-fun DI.MainBuilder.bindPriceRepository() {
     bind<PriceRepository>() with singleton { PriceRepository() }
+    // TODO extension functions
 }
-// TODO extension functions
